692ae857dc783f1aa74fa31f3f04b37c3dc8346a
TEERASAK <teerasak_no@hotmail.com>
2017-05-17 14:10:33 +0700

    react  11 edit

--- app/components/Main.js
+++ app/components/Main.js
@@ -8,19 +8,9 @@ var MainContainer = require('../containers/MainContainer');
 var Main = React.createClass({
     render: function () {
         return (
-            <MainContainer>
-
-                <h1>Github Battle</h1>
-
-                <p className='lead'>What even is a jQuery?</p>
-
-                <Link to='/playerOne'>
-
-                    <button type='button' className='btn btn-lg btn-success'>Get Started</button>
-
-                </Link >
-
-            </MainContainer >
+            <div>
+                {this.props.children}
+            </div>
         )
     }
 });
